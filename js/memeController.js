604b301856610b32748ea863ffaba4201a019dd6
itzhake <itzikil@icloud.com>
2022-10-26 19:51:06 +0300

    1ץ4

--- js/memeController.js
+++ js/memeController.js
@@ -72,3 +72,36 @@ function downloadMeme(elLink) {
     const imgContent = gElCanvas.toDataURL()
     elLink.href = imgContent
 }
+
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+    const reader = new FileReader()
+    reader.onload = function (event) {
+        let img = new Image()
+        img.src = event.target.result
+        img.onload = () => onImageReady(img)
+    }
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+function renderImg(img) {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function uploadImg(share) {
+    const imgDataUrl = gElCanvas.toDataURL()
+
+    function onSuccess(uploadedImgUrl) {
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        var url = `https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`
+        if (share) {
+            window.open(url)
+        } else {
+            alert(`Your Draw: ${uploadedImgUrl}`)
+        }
+    }
+    doUploadImg(imgDataUrl, onSuccess)
+}
