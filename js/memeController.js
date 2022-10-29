694978d5428c7e29cb828c728129d8525ac61100
itzhake <itzikil@icloud.com>
2022-10-29 21:52:07 +0300

    3.3

--- js/memeController.js
+++ js/memeController.js
@@ -214,3 +214,22 @@ function onShareImg(share) {
     }
     shareImg(imgDataUrl, onSuccess)
 }
+
+// const shareData = {
+//     title: 'MDN',
+//     text: 'Learn web development on MDN!',
+//     url: 'https://developer.mozilla.org'
+//   }
+  
+//   const btn = document.querySelector('.sharee');
+//   const resultPara = document.querySelector('.result');
+  
+//   // Share must be triggered by "user activation"
+//   btn.addEventListener('click', async () => {
+//     try {
+//       await navigator.share(shareData);
+//       resultPara.textContent = 'MDN shared successfully';
+//     } catch (err) {
+//       resultPara.textContent = `Error: ${err}`;
+//     }
+//   });
